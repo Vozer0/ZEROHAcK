4978a052b263a86e9aa614375c21a9ac3ca7bebf
David Vocero <vocerod004@gmail.com>
2025-07-05 13:19:44 -0700

    adding mean

--- HAcKTutorial.py
+++ HAcKTutorial.py
@@ -1,2 +1,9 @@
 print("hello, David & Danny")
-   
+   #me
+   #this is a mean function 
+def mean(x):
+    return sum(x) / len(x)
+
+# Example usage
+data = [1, 2, 3, 4, 5]
+print("Mean of data:", mean(data))
